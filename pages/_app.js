fb80a9661e2c40315114d7b507533fd18d06a39f
Neh Joshi <nehjoshi%5@gmail.com>
2022-04-12 16:56:57 +0530

    Form + UI changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import initFirebase from "../firebase/init";
+import { useEffect } from 'react';
 import "../styles/form.css";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    document.title = "CYSCOM | Cyber Defender";
+  }, [])
+
+
   initFirebase();
   return <Component {...pageProps} />;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,30 @@
 import { useState } from "react";
 import addToFirestore from "../firebase/add";
+import Logo from "../images/logo.png";
+import Image from "next/image";
 
 export default function Form() {
   const [formData, setFormData] = useState({});
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [registrationNumber, setRegistrationNumber] = useState("");
+  const [meal, setMeal] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
 
   const submitForm = async (e) => {
+    setLoading(true);
     e.preventDefault();
     try {
-      await addToFirestore(formData);
-      alert("Thank You For Registering!");
+      await addToFirestore({ name, email, registrationNumber, meal })
+      .then(() => {
+        setLoading(false);
+        setName("");
+        setEmail("");
+        setRegistrationNumber("");
+        setMeal("");
+        setSuccess(true);
+      })
     } catch (error) {
       console.log(error);
     }
@@ -16,17 +32,23 @@ export default function Form() {
 
   return (
     <>
-      <center>
-        <h1>Registration Form</h1>
-      </center>
-      <div className="wpcf7" id="wpcf7-f156-p143-o1 formwrap">
+      <nav>
+        <Image src={Logo} width={100} height={100} />
+      </nav>
+
+      <div className="wpcf7 " id="wpcf7-f156-p143-o1 formwrap">
+
         <form
           method="post"
-          className="wpcf7-form"
+          className="wpcf7-form main-form"
           noValidate
           onSubmit={submitForm}
         >
+          <center>
+            <h1>Sign up for Cyber Defender</h1>
+          </center>
           <span className="wpcf7-form-control-wrap Name">
+            <label className="label" for="Name">Name</label>
             <input
               type="text"
               name="Name"
@@ -36,11 +58,12 @@ export default function Form() {
               aria-invalid="false"
               placeholder="Name"
               onChange={(e) =>
-                setFormData({ ...formData, name: e.target.value })
+                setName(e.target.value)
               }
             />
           </span>
           <span className="wpcf7-form-control-wrap Email">
+            <label className="label" for="E-mail">VIT E-mail</label>
             <input
               type="email"
               name="Email"
@@ -50,11 +73,12 @@ export default function Form() {
               aria-invalid="false"
               placeholder="Email"
               onChange={(e) =>
-                setFormData({ ...formData, email: e.target.value })
+                setEmail(e.target.value)
               }
             />
           </span>
           <span className="wpcf7-form-control-wrap Name">
+            <label className="label" for="regno">Registration Number</label>
             <input
               type="text"
               name="Name"
@@ -62,13 +86,28 @@ export default function Form() {
               className="nameinput wpcf7-form-control wpcf7-text wpcf7-validates-as-required"
               aria-required="true"
               aria-invalid="false"
-              placeholder="Registartion Number"
+              placeholder="Registration Number"
               onChange={(e) =>
-                setFormData({ ...formData, registrationNumber: e.target.value })
+                setRegistrationNumber(e.target.value)
               }
             />
+            <label className="label">Meal preference</label>
+            <span className="wpcf7-form-control-wrap Name">
+              <select
+                className="mealinput wpcf7-form-control wpcf7-select"
+                name="Meal"
+                onChange={(e) =>
+                  setMeal(e.target.value)
+                }
+              >
+                <option value="">Select</option>
+                <option value="veg">Veg</option>
+                <option value="non-veg">Non-Veg</option>
+              </select>
+            </span>
+
           </span>
-          <span className="wpcf7-form-control-wrap Name">
+          {/* <span className="wpcf7-form-control-wrap Name">
             <input
               type="text"
               name="VITMail"
@@ -81,13 +120,33 @@ export default function Form() {
                 setFormData({ ...formData, vitMail: e.target.value })
               }
             />
-          </span>
-          <input
-            type="submit"
-            value="Send"
-            className="wpcf7-form-control wpcf7-submit btn"
-          />
+          </span> */}
+          {name === "" || email === "" || registrationNumber === "" || meal === "" && loading !== true && success !==true ? 
+            <input
+              type="submit"
+              value="Sign Up"
+              className="wpcf7-form-control wpcf7-submit btn-disabled"
+            /> : null
+          
+          }
+          {name !== "" && email !== "" && registrationNumber !== "" && meal !== "" && loading !== true && success!==true ?
+            <input
+              type="submit"
+              value="Sign Up"
+              className="wpcf7-form-control wpcf7-submit btn"
+            /> : null
+          }
+          {loading === true &&
+            <div className="loader">Loading...</div>}
+            {success === true && 
+            <p className="success-message">Thank you for registering!</p>}
         </form>
+
+        <footer class="footer">
+      <p>CYSCOM VITCC | Think before you click.</p>
+    </footer>
+
+
       </div>
     </>
   );
